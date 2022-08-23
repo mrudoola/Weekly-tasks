096f813a9f8bc38a34de104fef34a898f86731ff
mrudoola <47325781+mrudoola@users.noreply.github.com>
2022-08-23 16:49:06 +0530

    Add files via upload

---
+++ Mrudoola-Feb21-WT2/data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "How long does it take for glass to decompose",
+        "opt1" : "1 million years",
+        "opt2" : "1 billion years",
+        "opt3" : "100 years",
+        "answer" : "1 million years"
+    },
+    {
+        "q" : "Which country currently emits the most greenhouse gases",
+        "opt1" : "Russia",
+        "opt2" : "China",
+        "opt3" : "USA",
+        "answer" : "China"
+    },
+    {
+        "q" : "For every tone of paper recycled,How many trees are saved",
+        "opt1" : "59",
+        "opt2" : "32",
+        "opt3" : "17",
+        "answer" : "17"
+    },
+    {
+        "q" : "What is the theme of World Environment Day 2021?",
+        "opt1" : "Ecosystem Restoration",
+        "opt2" : "Restore Our Earth",
+        "opt3" : "Connecting People with Nature",
+        "answer" : "Ecosystem Restoration"
+    },
+    {
+        "q" : "The first World Environment Day was held in which year?",
+        "opt1" : "1974",
+        "opt2" : "1976",
+        "opt3" : "1971",
+        "answer" :"1974"
+    }
+];
---
+++ Mrudoola-Feb21-WT2/new_index.js
@@ -0,0 +1,32 @@
+var i = 0;
+var correctCount = 0 ;
+//initialize the first question
+generate(0);
+// generate from json array data with index
+function generate(index) {
+    document.getElementById("question").innerHTML = jsonData[index].q;
+    document.getElementById("optt1").innerHTML = jsonData[index].opt1;
+    document.getElementById("optt2").innerHTML = jsonData[index].opt2;
+    document.getElementById("optt3").innerHTML = jsonData[index].opt3;
+}
+
+function checkAnswer() {
+    if (document.getElementById("opt1").checked && jsonData[i].opt1 == jsonData[i].answer) {
+       correctCount++;
+    }
+    if (document.getElementById("opt2").checked && jsonData[i].opt2 == jsonData[i].answer) {
+        correctCount++;
+    }
+    if (document.getElementById("opt3").checked && jsonData[i].opt3 == jsonData[i].answer) {
+        correctCount++;
+    }
+    
+    i++;
+    if(jsonData.length-1 < i){
+
+        document.write("<body style='background-color:wheat;'>");
+        document.write("<div style='color:black;font-size:18pt;text-align:center;margin-top:200px'>Your score is : "+correctCount+"/5</div>");
+        document.write("</body>");
+    }
+    generate(i);
+}
